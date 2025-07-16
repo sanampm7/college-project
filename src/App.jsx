44637ef55d6d7efa13b10b79d73eda87f0a93e45
sanampm7 <sanampoon27@gmail.com>
2025-07-16 16:01:49 +0545

    About the header section of Movie Ticket Booking App

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Favorite from "./pages/Favorite";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Movies from "./pages/Movies";
+import MyBookings from "./pages/MyBookings";
+import SeatLayout from "./pages/SeatLayout";
+import { Toaster } from "react-hot-toast";
+import Footer from "./components/Footer";
+
+const App = () => {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
+
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
